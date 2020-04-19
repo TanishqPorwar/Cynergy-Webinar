f21277e007e2d8da3fbffc47debdac95981f88f6
Tanishq <tanishqporwar22@gmail.com>
2020-04-19 11:51:38 +0530

    week3/cops

--- week 3/COPS.py
+++ week 3/COPS.py
@@ -1,21 +1,22 @@
 # https://www.codechef.com/problems/COPS
 
+
 for _ in range(int(input())):
     m, x, y = map(int, input().split())
-    # print(m, x, y)
+    arr = [True for i in range(0, 101)]
     a = list(map(int, input().split()))
-    a = [1]+sorted(a)+[100]
-    safe = 0
-    N = len(a)
     radius = x*y
-    for i in range(1, N-1):
-        if i == 1:
-            temp = a[i] - a[i-1] - radius
-            safe += (temp >= 0) * temp
-            temp = (a[i+1]-a[i]-2*radius-1)
-        elif i == N-2:
-            temp = a[i+1]-a[i] - radius
-        else:
-            temp = (a[i+1]-a[i]-2*radius-1)
-        safe += (temp >= 0) * temp
-    print(safe)
+    for i in a:
+        end = i + radius
+        start = i - radius
+
+        if start < 1:
+            start = 1
+
+        if end > 100:
+            end = 100
+        # print(start, end)
+        for j in range(start, end+1):
+            arr[j] = False
+    print(arr.count(True)-1)
+    # print("...")
